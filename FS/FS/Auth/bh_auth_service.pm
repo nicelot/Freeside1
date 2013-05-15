7cc862e180209f1cddee50eae66243dc9e914646
Jason (Jayce^) Hall <jayce@lug-nut.com>
2013-05-15 11:58:10 -0600

    Add first version of an auth plugin that validates against roster
    Still has todo's for the templated role for new users and checking a
    roster based permission for logging in

---
+++ FS/FS/Auth/bh_auth_service.pm
@@ -0,0 +1,41 @@
+package FS::Auth::bh_auth_service;
+
+use strict;
+use base qw( FS::Auth::external );
+use lib '/var/skynet/lib';
+use config;
+use API::Client;
+
+sub authenticate {
+  my($self, $username, $check_password, $info ) = @_;
+
+  my $c=API::Client->new({service=>"emp_auth"});
+  use Debug;
+  my $ret = $c->verify_pass(
+    {
+      user => $username,
+      pass=> $check_password,
+      emp_info => 1,
+    }
+  );
+  if(!$ret) {
+    return 0;
+  }
+
+  if( $ret->{data}->{'valid'} == 1 ) {
+    my @name = split(/\s/, $ret->{data}->{name} );
+
+    ##TODO: check for a 'freeside' or billing interface permission:
+    #
+    $info->{'first'}  = shift @name;
+    $info->{'last'}  = "@name";
+    #$info->{'template_user'} = '';  #TODO: some lookup in the roster data to see what permissions they need
+    return 1;
+  } else {
+    ## bad login
+    return 0;
+  }
+  return $ret;
+}
+
+1;
